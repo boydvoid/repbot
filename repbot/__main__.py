6ed3ed412e03d0e3f1435d19b373876fb5eacad9
Bobby Boyd <bboyd2008@gmail.com>
2019-06-23 15:22:45 -0700

    Done

--- repbot/__main__.py
+++ repbot/__main__.py
@@ -10,6 +10,7 @@ import os
 import platform
 import json
 
+
 def main():
 
     questions = [
@@ -26,17 +27,18 @@ def main():
     ]
     answers = prompt(questions)
 
-    print(answers)
-
     if answers['menu'] == 'Create a new repo':
+        print('Create repo...')
         createRepo()
     elif answers['menu'] == 'Change my settings':
-        setup(name, password, directory)
+        print('Run setup...')
+
+        runSetup()
     else:
         exit()
 
 
-def createRepo() :
+def createRepo():
     if path.exists("data.txt"):
         questions = [
             {
@@ -66,57 +68,49 @@ def createRepo() :
         u = g.get_user()
 
         create(title, private, u, directory)
-        main()
-
-    else:
-        questions = [
-            {
-                'type': 'input',
-                'name': 'username',
-                'message': 'What is your Github Username?'
-            },
-            {
-                'type': 'password',
-                'name': 'password',
-                'message': 'What is your Github Password'
-            },
-            {
-                'type': 'input',
-                'name': 'directory',
-                'message': 'What directory would you like to clone your repos into? *Enter the full directory ie. C:/Users/../../..'
-            },
-        ]
-        answers = prompt(questions)
-        setup(answers['username'], answers['password'], answers['directory'])
 
         questions = [
-            {
-                'type': 'input',
-                'name': 'repo_name',
-                'message': 'Name your repo'
-            },
             {
                 'type': 'confirm',
-                'name': 'private',
-                'message': 'Is this repo private? '
+                'name': 'open',
+                'message': 'Do you want to open the workspace?'
             }
         ]
+
         answers = prompt(questions)
 
-        title = answers['repo_name']
-        private = answers['private']
+        if answers['open'] == 'Yes':
+            # open
+        else:
+            main()
 
-        with open('data.txt') as json_file:
-            data = json.load(json_file)
-            for p in data['account']:
-                user = p['username']
-                password = p['password']
-                directory = p['directory']
+    else:
+        runSetup()
 
-        g = Github(user, password)
-        u = g.get_user()
 
-        create(title, private, u, directory)
+def runSetup():
+    questions = [
+        {
+            'type': 'input',
+            'name': 'username',
+            'message': 'What is your Github Username?'
+        },
+        {
+            'type': 'password',
+            'name': 'password',
+            'message': 'What is your Github Password'
+        },
+        {
+            'type': 'input',
+            'name': 'directory',
+            'message': 'What directory would you like to clone your repos into? *Enter the full directory ie. C:/Users/../../..'
+        },
+    ]
+    answers = prompt(questions)
+    setup(answers['username'], answers['password'], answers['directory'])
+
+    main()
+
 
 if __name__ == '__main__':
     main()
